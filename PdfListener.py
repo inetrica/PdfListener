698115c13b62d6c14afa3e12e0fa2836d549e58b
inetrica <inetrica@gmail.com>
2016-01-18 16:13:41 -0600

    moves files to folders, create necessary folders on setup

--- PdfListener.py
+++ PdfListener.py
@@ -7,12 +7,24 @@ trash = "trash/"
 
 def spinloop(dstpath):
     while True:
-        for f in os.listdir(srcpath):
-            print(f)
+        for f in os.listdir(srcpath): #for each file in folder
+            s = os.path.join(srcpath, f)
+            if f.endswith(".pdf"): #move to designated folder
+                d = os.path.join(dstpath, f)
+                os.rename(s, d)
+            else:                  #move to trash folder
+                d = os.path.join(trash, f)
+                os.rename(s, d)
         time.sleep(5)
 
-def setup():
+def setup(dstpath):
+    #make sure src exists
     directory = os.path.dirname(srcpath)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    #make sure dst exists
+    directory = os.path.dirname(dstpath)
     if not os.path.exists(directory):
         os.makedirs(directory)
 
