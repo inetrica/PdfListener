f2cd0d7b77700b61b20ea099da344af6bb881159
inetrica <inetrica@gmail.com>
2016-01-18 15:52:39 -0600

    trash folder for non pdfs, while loop listening in data/, not yet processing files

--- PdfListener.py
+++ PdfListener.py
@@ -1,6 +1,33 @@
+import sys
 import os
+import time
+
+srcpath = "data/"
+trash = "trash/"
+
+def spinloop(dstpath):
+    while True:
+        for f in os.listdir(srcpath):
+            print(f)
+        time.sleep(5)
+
+def setup():
+    directory = os.path.dirname(srcpath)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python PdfListener.py <dst path>")
+        exit()
+
+    setup()
+
+    #make sure folder to listen from exists
+    dstloc = sys.argv[1]
+    print(dstloc)
+
+    spinloop(dstloc)
 
 
 if __name__ == "__main__":
